28b12290721e9d57ef9aaabbedbad244af37710c
Junghyun her <freenham@gmail.com>
2017-06-05 19:09:21 +0900

    보트클래스 생성자

---
+++ PERDUE/src/june5th/Boat.java
@@ -0,0 +1,22 @@
+package june5th;
+
+public class Boat {
+	private int team;
+	private Coordinates location;
+	private int direction;
+	private int health;
+	private int strength;
+	private int vision;
+	
+	public Boat(int team, Coordinates location, int direction, int health, int strength, int vision) {
+		this.team = team;
+		this.location = location;
+		this.direction = direction;
+		this.health = health;
+		this.strength = strength;
+		this.vision = vision;
+	}
+	
+	
+
+}
