f185f273e88f42ed7999a67fd241ffeea7556e52
Junghyun her <freenham@gmail.com>
2017-06-05 18:35:38 +0900

    Coordiantes 완성
    메인 함수 제거

--- PERDUE/src/june5th/Coordinates.java
+++ PERDUE/src/june5th/Coordinates.java
@@ -3,37 +3,32 @@ package june5th;
 public class Coordinates {
 	private int x;
 	private int y;
-	
+
 	public Coordinates() {
-		x=0;
-		y=0;
+		x = 0;
+		y = 0;
 	}
-	
-	public Coordinates(int x,int y) {
-		this.x =x;
-		this.y =y;
+
+	public Coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
 	}
-	
+
 	public int getX() {
 		return x;
 	}
-	
+
 	public int getY() {
 		return y;
 	}
-	
+
 	public void setCoordinates(int x, int y) {
-		this.x=x;
-		this.y=y;
-	}
-	
-	public String toString() {
-		return String.format("\"%c%c\" // x = %d, y = %d", (char)(y+65),(char)(x+48),x,y);
-	}
-	
-	public static void main(String[] args) {
-		Coordinates c = new Coordinates(7,2);
-		System.out.println(c);
+		this.x = x;
+		this.y = y;
 	}
 
+	public String toString() {
+		return String.format("\"%c%c\" // x = %d, y = %d", (char) (y + 65), (char) (x + 48), x, y);
+
+	}
 }
