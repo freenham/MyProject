0dc9f958ea640e7f4c20b6697c8f445306f3f944
Junghyun her <freenham@gmail.com>
2017-06-12 14:27:45 +0900

    2차수정

--- PERDUE/src/june5th/World.java
+++ PERDUE/src/june5th/World.java
@@ -20,7 +20,7 @@ public class World {
 			height = 4;
 		else if (height >= 10)
 			height = 10;
-		map = new Object[height][width];
+		map = new Object[height + 1][width + 1];
 
 		for (int i = 0; i < map.length; i++) {
 			for (int j = 0; j < map[0].length; j++) {
@@ -89,72 +89,159 @@ public class World {
 	}
 
 	public String drawTeamMap(Boat[] b, int viewType) {
+		// Boolean flag = false;
+		// String s = "";
+		// if (viewType == 1) {
+		//
+		// for (int y = 0; y <= getHeight(); y++) {
+		// s += "" + (char) (64 + y) + " ";
+		// for (int x = 0; x < getWidth(); x++) {
+		// if (y == 0) {
+		// s += " " + (x + 1) + " ";
+		// } else if (y != 0) {
+		// s += "###";
+		// }
+		// }
+		// s += "\n";
+		// }
+		// }
+		//
+		// else if (viewType == 2) {
+		//
+		// for (int y = 0; y <= getHeight(); y++) {
+		// s += "" + (char) (64 + y) + " ";
+		// for (int x = 0; x < getWidth(); x++) {
+		// if (y == 0) {
+		// s += " " + (x + 1) + " ";
+		// }
+		// flag = true;
+		// for (int i = 0; i < b.length; i++) {
+		// if (x == b[i].getLocation().getX() - 1 && y ==
+		// b[i].getLocation().getY()) {
+		// s += "" + b[i].getDirection() + b[0].getID() + b[i].getTeam();
+		// flag = false;
+		// } else if ((x >= b[i].getLocation().getX() - 1 - b[i].getVision()
+		// && x <= b[i].getLocation().getX() - 1 + b[i].getVision())
+		// && (y >= b[i].getLocation().getY() - b[i].getVision()
+		// && y <= b[i].getLocation().getY() + b[i].getVision())) {
+		// s += "~~~";
+		// flag = false;
+		// }
+		// }
+		// if (flag && y!=0) {
+		// s += "###";
+		// flag = true;
+		// }
+		//
+		// }
+		// s += "\n";
+		// }
+		// } else if (viewType == 3) {
+		//
+		// for (int y = 0; y <= getHeight(); y++) {
+		// s += "" + (char) (64 + y) + " ";
+		// for (int x = 0; x < getWidth(); x++) {
+		// if (y == 0) {
+		// s += " " + (x + 1) + " ";
+		// } else if (x == b[0].getLocation().getX() - 1 && y ==
+		// b[0].getLocation().getY()) {
+		// s += "" + b[0].getHealth() + b[0].getID() + b[0].getTeam();
+		// } else if ((x >= b[0].getLocation().getX() - 1 - b[0].getVision()
+		// && x <= b[0].getLocation().getX() - 1 + b[0].getVision())
+		// && (y >= b[0].getLocation().getY() - b[0].getVision()
+		// && y <= b[0].getLocation().getY() + b[0].getVision())) {
+		// s += "~~~";
+		// } else if (y != 0) {
+		// s += "###";
+		// }
+		// }
+		// s += "\n";
+		// }
+		//
+		// }
+		// return s;
+		Boolean flag = false;
 		String s = "";
 		if (viewType == 1) {
-
-			for (int y = 0; y <= getHeight(); y++) {
-				s += "" + (char) (64 + y) + " ";
+			for (int y = 0; y < getHeight(); y++) {
 				for (int x = 0; x < getWidth(); x++) {
-					if (y == 0) {
-						s += " " + (x + 1) + " ";
-					} else if (y != 0) {
-						s += "###";
+					map[x][y] = "###";
+					if (x == 0)
+						map[x][y] = "" + (char) (64 + y) + " ";
+					else if (y == 0)
+						map[x][y] = " " + (x) + " ";
+				}
+			}
+		} else if (viewType == 2) {
+			for (int y = 0; y < getHeight(); y++) {
+				for (int x = 0; x < getWidth(); x++) {
+					for (int i = 0; i < b.length; i++) {
+						if (map[x][y] == null)
+							map[x][y] = "###";
+						if (x == 0)
+							map[x][y] = "" + (char) (64 + y) + " ";
+						else if (y == 0)
+							map[x][y] = " " + (x) + " ";
+						else if (x == b[i].getLocation().getX() && y == b[i].getLocation().getY()) {
+							map[x][y] = "" + b[i].getDirection() + b[i].getID() + b[i].getTeam();
+						} else if ((x >= b[i].getLocation().getX() - b[i].getVision()
+								&& x <= b[i].getLocation().getX() + b[i].getVision())
+								&& (y >= b[i].getLocation().getY() - b[i].getVision()
+										&& y <= b[i].getLocation().getY() + b[i].getVision())) {
+							if (map[x][y].equals("###"))
+								map[x][y] = "~~~";
+						}
 					}
 				}
-				s += "\n";
+			}
+		}
+		else if (viewType == 3) {
+			for (int y = 0; y < getHeight(); y++) {
+				for (int x = 0; x < getWidth(); x++) {
+					for (int i = 0; i < b.length; i++) {
+						if (map[x][y] == null)
+							map[x][y] = "###";
+						if (x == 0)
+							map[x][y] = "" + (char) (64 + y) + " ";
+						else if (y == 0)
+							map[x][y] = " " + (x) + " ";
+						else if (x == b[i].getLocation().getX() && y == b[i].getLocation().getY()) {
+							map[x][y] = "" + b[i].getHealth() + b[i].getID() + b[i].getTeam();
+						} else if ((x >= b[i].getLocation().getX() - b[i].getVision()
+								&& x <= b[i].getLocation().getX() + b[i].getVision())
+								&& (y >= b[i].getLocation().getY() - b[i].getVision()
+										&& y <= b[i].getLocation().getY() + b[i].getVision())) {
+							if (map[x][y].equals("###"))
+								map[x][y] = "~~~";
+						}
+					}
+				}
 			}
 		}
 
-		else if (viewType == 2) {
-
-			for (int y = 0; y <= getHeight(); y++) {
-				s += "" + (char) (64 + y) + " ";
-				for (int x = 0; x < getWidth(); x++) {
-					if (y == 0) {
-						s += " " + (x + 1) + " ";
-					} else if (x == b[0].getLocation().getX() - 1 && y == b[0].getLocation().getY()) {
-						s += "" + b[0].getDirection() + b[0].getID() + b[0].getTeam();
-					} else if ((x >= b[0].getLocation().getX() - 1 - b[0].getVision()
-							&& x <= b[0].getLocation().getX() - 1 + b[0].getVision())
-							&& (y >= b[0].getLocation().getY() - b[0].getVision()
-									&& y <= b[0].getLocation().getY() + b[0].getVision())) {
-						s += "~~~";
-					} else if (y != 0) {
-						s += "###";
-					}
-				}
-				s += "\n";
+		for (int y = 0; y < getHeight(); y++) {
+			for (int x = 0; x < getWidth(); x++) {
+				s += map[x][y];
 			}
-		} else if (viewType == 3) {
-
-			for (int y = 0; y <= getHeight(); y++) {
-				s += "" + (char) (64 + y) + " ";
-				for (int x = 0; x < getWidth(); x++) {
-					if (y == 0) {
-						s += " " + (x + 1) + " ";
-					} else if (x == b[0].getLocation().getX() - 1 && y == b[0].getLocation().getY()) {
-						s += "" + b[0].getHealth() + b[0].getID() + b[0].getTeam();
-					} else if ((x >= b[0].getLocation().getX() - 1 - b[0].getVision()
-							&& x <= b[0].getLocation().getX() - 1 + b[0].getVision())
-							&& (y >= b[0].getLocation().getY() - b[0].getVision()
-									&& y <= b[0].getLocation().getY() + b[0].getVision())) {
-						s += "~~~";
-					} else if (y != 0) {
-						s += "###";
-					}
-				}
-				s += "\n";
-			}
-
+			s += "\n";
 		}
 		return s;
 	}
 
 	public static void main(String[] args) {
 		World w = new World(10, 10);
-		Boat[] b = new Boat[1];
-		Boat bs = new BattleShip(1, new Coordinates(1, 1), World.NORTH, 5, 2, 1);
-		b[0] = bs;
-		System.out.println(w.drawTeamMap(b, 1));
+		Boat[] b = new Boat[5];
+		Boat D1 = new BattleShip(1, new Coordinates(10, 2), World.NORTH, 2, 2, 1);
+		Boat C1 = new BattleShip(1, new Coordinates(6, 6), World.NORTH, 3, 3, 3);
+		Boat S1 = new BattleShip(1, new Coordinates(2, 8), World.NORTH, 3, 3, 2);
+		Boat A1 = new BattleShip(1, new Coordinates(5, 8), World.NORTH, 5, 5, 0);
+		Boat B1 = new BattleShip(1, new Coordinates(5, 10), World.NORTH, 4, 4, 1);
+
+		b[0] = D1;
+		 b[2] = C1;
+		 b[3] = S1;
+		 b[4] = A1;
+		 b[1] = B1;
+		System.out.println(w.drawTeamMap(b, 3));
 	}
 }
